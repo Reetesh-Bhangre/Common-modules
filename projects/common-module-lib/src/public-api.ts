608c14702a0eb387022cc787647a6cc327f25eaa
Reetesh Bhangre <rbhangre@delaplex.com>
2022-05-20 13:54:49 +0530

    exported component and module via public-api to solve build error

--- projects/common-module-lib/src/public-api.ts
+++ projects/common-module-lib/src/public-api.ts
@@ -7,3 +7,7 @@ export * from './lib/grid/grid.module';
 export * from './lib/grid/grid.component';
 export * from './lib/custom-input/custom-input.component';
 export * from './lib/custom-input/custom-input.module';
+export * from './lib/custom-date-picker/custom-date-picker.component';
+export * from './lib/custom-date-picker/custom-date-picker.module';
+export * from './lib/control-value-accessor-form/control-value-accessor-form.component';
+export * from './lib/control-value-accessor-form/control-value-accessor-form.module';
